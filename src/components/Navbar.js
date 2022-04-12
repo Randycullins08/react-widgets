c87b5ad8623c014f49203ac2cfc9a4c14fba2aa9
Randy Cullins <randycullins2018@gmail.com>
2022-04-12 06:06:31 -0600

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+export default function Navbar() {
+  return (
+    <div className="navbar-container">
+      <div className="logo-wrapper">
+        <h1>React Widgets</h1>
+      </div>
+
+      <div className="links-wrapper">
+        <button>Widget</button>
+        <button>Widget</button>
+        <button>Widget</button>
+        <button>Widget</button>
+        <button>Widget</button>
+        <button>Widget</button>
+        <button>Widget</button>
+      </div>
+    </div>
+  );
+}
